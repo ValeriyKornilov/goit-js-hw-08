54aa9babe1507decb784310e7ded2be04174c8b6
wellkornilov <kornilovaleriy@gmail.com>
2022-11-28 22:18:00 +0200

    28112222

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,55 +1,42 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-from-state';
 
-
-
 const refs = {
-    form: document.querySelector('.feedback-form'),
+  form: document.querySelector('.feedback-form'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 function getLsData() {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-};
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+}
 
 function onFormInput(e) {
-    const formData = getLsData();
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
-};
-
+  const formData = getLsData();
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
 function fillForm() {
-    const formData = getLsData();
+  const formData = getLsData();
 
-    for (let key in formData) {
-        refs.form.elements[key].value = formData[key];
-    }
+  for (let key in formData) {
+    refs.form.elements[key].value = formData[key];
+  }
 }
 
 fillForm();
 
 function onFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(e.target);
+  const formData = new FormData(e.target);
 
-    const formDataObj = Object.fromEntries(formData.entries());
-    console.log(formDataObj);
+  const formDataObj = Object.fromEntries(formData.entries());
+  console.log(formDataObj);
 
-
-    // const myFormData = new FormData(event.target);
-
-    // const formDataObj = {};
-    // myFormData.forEach((value, key) => (formDataObj[key] = value));
-    // console.log(formDataObj);
-
-
-    localStorage.removeItem(STORAGE_KEY);
-    e.currentTarget.reset();
-};
-
-
+  localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
+}
